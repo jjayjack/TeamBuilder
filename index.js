dea771f221060cbce2c479237323d39868d8c27c
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2021-04-27 16:15:40 -0500

    initial commit of code

---
+++ index.js
@@ -0,0 +1,35 @@
+class Employee {
+    constructor (name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName();
+    getId();
+    getEmail();
+    getRole();
+};
+
+class Manager extends Employee {
+    constructor (officeNumber) {
+        getRole()
+            return Manager;
+    }
+};
+
+class Engineer extends Employee{
+    constructor (github) {
+        getGithub();
+        this.getRole()
+            return Engineer;
+    }
+};
+
+class Intern extends Employee{
+    constructor (school) {
+        const school
+        getSchool();
+        getRole()
+            return Intern;
+    }
+};
