ef479239eb9575a63a4a865354e75617f66755b4
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2021-04-27 21:00:39 -0500

    Updated code to create HTML

--- dist/team.js
+++ dist/team.js
@@ -7,10 +7,35 @@ const generateTeam = (data) => {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Our Team</title>
     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
 <body>
-    
+    <div class="container-fluid">
+       ${data}
+    </div>
 </body>
 </html>`
 }
+
+function link(teamMem){
+    let boots = "";
+    teamMem.forEach(member => {
+        boots += card(member);
+    });
+    return generateTeam(boots);
+    
+}
+const card = (info) => {
+    return `<div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${info.title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${info.name}</h6>
+                <p class="card-text">${info.id}</p>
+                <p class="card-text">${info.email}</p>
+                <p>${info.office}</p>
+                <p>${info.github}</p>
+                <p>${info.school}</p>
+            </div>
+        </div>`
+}
+module.exports = link;
--- src/builder.js
+++ src/builder.js
@@ -1,17 +1,106 @@
 const inquirer = require('inquirer')
-const employee = require ('./lib/Employee');
-const engineer = require ('./lib/Engineer');
-const intern = require('./lib/Intern');
-const manager = require('./lib/Manager');
-const teamGen = require('./dist/team.js')
+// const employee = require ('../lib/Employee');
+// const engineer = require ('../lib/Engineer');
+// const intern = require('../lib/Intern');
+// const manager = require('../lib/Manager');
+const teamGen = require('../dist/team.js')
+const teamMem = [];
+const fs = require('fs');
 
-inquirer
+const questions = [
+    {
+        type: 'list',
+        name: 'title',
+        message: "What is your position?",
+        choices: ['Engineer', 'Intern', 'Manager', 'None to add'],
+        default: 'Employee',
+    },
+    {
+        type: 'name',
+        name: 'name',
+        message: "What is your name?",
+        when: function(data){
+            if(data.title == 'None to add'){
+                return false;
+            } return true
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is your id?",
+        when: function(data){
+            if(data.title == 'None to add'){
+                return false;
+            } return true
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email?",
+        when: function(data){
+            if(data.title == 'None to add'){
+                return false;
+            } return true
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub?',
+        when: function (data) {
+            if (data.title == 'Engineer') {
+                return true;
+            };
+            return false;
+        }
+    },
+    {
+        type: 'input',
+        name: 'office',
+        message: 'What is your Office location?',
+        when: function (data) {
+            if (data.title == 'Manager') {
+                return true;
+            };
+            return false;
+        }
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What school do you attend?',
+        when: function (data) {
+            if (data.title == 'Intern') {
+                return true;
+
+            };
+            return false;
+        }
+    },
+
+]
+
+function addTeam() {
+    inquirer
         .prompt(questions)
-        .then((response) =>
-            writeToFile('createdTeam.html', teamGen(response))
-        )
-class Team {
-    constructor(){
-        
-    }
+        .then((response) => {
+            if (response.title !== "None to add") {
+                teamMem.push(response);
+                console.log(teamMem);
+                addTeam();
+            } else {
+                writeToFile('createdTeam.html', teamGen(teamMem))
+            }
+        })
+};
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data, function(err){
+        if (err) return console.log(err);
+        console.log(data);
+    })
 }
+
+addTeam();
